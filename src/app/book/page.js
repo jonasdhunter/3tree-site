ca7a104674abeb30c0801d45f086b28e749d33c8
Jonas Hunter <jonasdhunter@gmail.com>
2025-07-03 19:22:52 -0500

    remaining fixes

--- src/app/book/page.js
+++ src/app/book/page.js
@@ -72,9 +72,9 @@ export default function Book() {
         <div className="max-w-[600px] mx-auto">
           {!submitted ? (
             <>
-              <h1 className="text-[2.5rem] leading-tight font-semibold mb-4 text-center">Let's Connect</h1>
+              <h1 className="text-[2.5rem] leading-tight font-semibold mb-4 text-center">Let&apos;s Connect</h1>
               <p className="text-xl text-[#95A5A6] mb-12 text-center">
-                Share a bit about what you're working on and we'll find a time to explore how we might support you.
+                Share a bit about what you&apos;re working on and we&apos;ll find a time to explore how we might support you.
               </p>
 
               <form onSubmit={handleSubmit} className="space-y-6">
